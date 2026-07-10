f8fc3e0a797f21990752f0e72cd9c7d7ba67ca7c
kodaicoder <nutdeknon@gmail.com>
2026-07-10 12:10:10 +0700

    feat: set up flat export API
    - Create src/lib/index.ts with all component exports
    - Export atoms: Button, Input, Badge, Label
    - Export molecules: FormField, SearchBar
    - Export organisms: Dialog (namespace)
    - Export utils: cn
    
    Completes ticket #11 (flat export API)

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,15 @@
-// place files you want to import through the `$lib` alias in this folder.
+// Atoms
+export { Button, buttonVariants, type ButtonProps, type ButtonVariant, type ButtonSize } from './atoms/button/index.js';
+export { Input } from './atoms/input/index.js';
+export { Badge, badgeVariants } from './atoms/badge/index.js';
+export { Label } from './atoms/label/index.js';
+
+// Molecules
+export { FormField } from './molecules/form-field/index.js';
+export { SearchBar } from './molecules/search-bar/index.js';
+
+// Organisms
+export * as Dialog from './organisms/dialog/index.js';
+
+// Utils
+export { cn } from './utils/index.js';
